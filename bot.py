6e219732e9e45027b47cdae70f81a0273177f7d4
ALEKSEY <88752711+DrFess@users.noreply.github.com>
2023-12-12 23:17:55 +0800

    изменил текст сообщения

--- bot.py
+++ bot.py
@@ -1,5 +1,3 @@
-import os
-
 import asyncio
 import logging
 import aioschedule
@@ -11,8 +9,7 @@ from aiogram.types import Message
 from utils import daily_summary
 
 
-print(os.getenv('TOKEN'))
-bot = Bot(token=os.getenv('TOKEN'), parse_mode='HTML')
+bot = Bot(token='6716211777:AAGJrwvEVLodkQco1VEQNzXx6MheUDXPy1k', parse_mode='HTML')
 router = Router()
 
 
@@ -26,28 +23,22 @@ async def send_daily_report():
     data = daily_summary()
     if data['экстренных госпитализаций'] == 0:
         text = f'Всего обратилось: {data["всего обратилось"]}\n' \
-               f'Экстренных госпитализация: {data["экстренных госпитализаций"]}'
+               f'Экстренных госпитализаций: {data["экстренных госпитализаций"]}'
     else:
         hospitalization = ''
         for item in data['госпитализации']:
-            hospitalization.join(f'Номер истории: {item[0]}\n'
-                                 f'Диагноз: {item[1]}\n'
-                                 f'Примечание: {item[2]}\n')
-
+            hospitalization += f'\n------\nНомер истории: {item[0]}\nДиагноз: {item[1]}\nПримечание: {item[2]}'
         text = f'Всего обратилось: {data["всего обратилось"]}\n' \
-               f'Экстренных госпитализация: {data["экстренных госпитализаций"]}\n' \
+               f'Экстренных госпитализаций: {data["экстренных госпитализаций"]}\n' \
                f'Сведения о госпитализациях: {hospitalization}'
-    await bot.send_message(chat_id=os.getenv('GROUP_ID'), text=text)
+    await bot.send_message(chat_id=-1001396112169, text=text)
 
 
 async def scheduler():
-    aioschedule.every().day.at('17:45').do(send_daily_report)
+    aioschedule.every().day.at('23:15').do(send_daily_report)
     while True:
         await aioschedule.run_pending()
-        await asyncio.sleep(1)
-
-# async def on_startup(dp):
-#     asyncio.create_task(scheduler())
+        await asyncio.sleep(5)
 
 
 async def main():
