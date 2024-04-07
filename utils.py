98675c0b2ae60ed1d44ec9066ab91d18e2ad613b
ALEKSEY <88752711+DrFess@users.noreply.github.com>
2024-04-07 11:49:19 +0800

    edit clear duty

--- utils.py
+++ utils.py
@@ -148,7 +148,7 @@ def clear_duty_schedule():
     """Очищает заданный диапазон таблицы"""
     sh = connect_to_google_sheets()
     worksheet = sh.get_worksheet_by_id(735128125)
-    worksheet.batch_clear(['C1:AG21'])
+    worksheet.batch_clear(['C1:AG24'])
     worksheet.format(
         'C1:AG21',
         {'backgroundColor':
